e72569cf7560e6b8cc8a9f3a7667f0c6473970d2
fredkneeland <fred@bladelinux.com>
2024-04-26 18:20:05 +0300

    launch screen stuff

---
+++ lib/splash_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:async';
+
+import 'package:kardiology_flutter/HomePage.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({
+    super.key,
+  });
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(
+      Duration(seconds: 3),
+      () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomePage()),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset(
+          'assets/images/cover_white.png',
+        ),
+      ),
+    );
+  }
+}
